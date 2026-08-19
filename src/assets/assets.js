32c54f984dd7dd5186f4f2770b68c4c4ce5fd9e5
akajdid-mostafa <mostafaakajdid6@gmail.com>
2026-08-19 17:54:37 +0100

    updat resume

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,7 +2,7 @@ import scroll from "./scroll.svg";
 import send from "./send.svg";
 import about from "./about.webp";
 import profile from "./mostafa.webp";
-import resume from "./mostafa-akajdid-cv.pdf";
+import resume from "./Mostafa_Akajdid_Full_Stack_Developer_Resume_en.pdf";
 
 export {
   send,
